de51df7e0ec8eddc7cecadccb3a74e27d30619f4
AgentKisa <shapoval.ads@gmail.com>
2025-08-29 13:50:00 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,29 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import { globSync } from "glob";
-import injectHtml from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
+import { defineConfig } from 'vite';
+import { globSync } from 'glob';
+import injectHtml from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => ({
-  // 1) Говорим Vite, что «корнем» является папка src
-  root: "src",
+  // !!!
+  base: '/shapoval-agency/',
 
-  // 2) Куда выкладывать собранный build
+  root: 'src',
+
   build: {
-    outDir: "../dist", // на уровень выше, чтобы src осталось чистым
+    outDir: '../dist',
     emptyOutDir: true,
     sourcemap: true,
 
-    // 3) Собираем каждую HTML-страницу из src как отдельную точку входа
     rollupOptions: {
-      input: globSync("./src/*.html"),
+      input: globSync('./src/*.html'),
       output: {
-        // складываем сторонние либы в vendor.js
         manualChunks(id) {
-          if (id.includes("node_modules")) return "vendor";
+          if (id.includes('node_modules')) return 'vendor';
         },
-        entryFileNames: "commonHelpers.js",
+        entryFileNames: 'commonHelpers.js',
       },
     },
   },
 
-  plugins: [
-    injectHtml(), // склейка <load />
-    FullReload(["./src/**/*.html"]), // live-reload всех html
-  ],
+  plugins: [injectHtml(), FullReload(['./src/**/*.html'])],
 }));
